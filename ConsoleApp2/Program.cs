1ca9caa64fb14b7fe22488affd0e2867c813ee4d
Torriandr <torri_alex_3705@vk.com>
2024-04-04 13:56:14 +0700

    Added sort text function

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Sort(char[] str)
+    {
+        for (int i = 0; i < str.Length - 1; ++i)
+        {
+            for (int j = i + 1; j < str.Length; ++j)
+            {
+                if (String.CompareOrdinal(str[i].ToString(), str[j].ToString()) > 0)
+                {
+                    (str[i], str[j]) = (str[j], str[i]);
+                }
+            }
+        }
+    }
+    
+    public static void Main()
+    {
+        Console.WriteLine("Введите любую строку:");
+        char[] str = Console.ReadLine().ToCharArray();
+        Sort(str);
+        Console.WriteLine(str);
+    }
+}
