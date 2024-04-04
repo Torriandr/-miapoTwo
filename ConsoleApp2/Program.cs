2de39d6ec23f0fa6e3b77b38d14afbec6e82cd00
Torriandr <torri_alex_3705@vk.com>
2024-04-04 13:42:28 +0700

    Added sum function

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Sum(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+    public static void Main()
+    {
+        int a = 0;
+        int b = 0;
+        Console.Write("Введите первое число: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        b = int.Parse(Console.ReadLine());
+        Sum(a, b);
+    }
+}
